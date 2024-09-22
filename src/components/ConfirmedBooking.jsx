3a38d7d3fb789f1271a3388b82f6f720f1397244
ChristopherChantres <chris.chantres@gmail.com>
2024-09-22 10:26:54 -0600

    ConfirmedBooking component created

---
+++ src/components/ConfirmedBooking.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../css/ConfirmedBooking.css'
+
+const ConfirmedBooking = () => {
+  return (
+    <div className="confirmation-page">
+      <div className="confirmation-container">
+        <h2>Booking Confirmed!</h2>
+        <p>Thank you for your reservation. We look forward to seeing you soon!</p>
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmedBooking;
